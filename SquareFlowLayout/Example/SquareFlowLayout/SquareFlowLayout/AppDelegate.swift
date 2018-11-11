e0c16ea572a468dba0018c43a8232d040a14a9e9
Taras Chernyshenko <taras.chernyshenko@gmail.com>
2018-11-11 11:26:38 +0200

    - init commit

---
+++ SquareFlowLayout/Example/SquareFlowLayout/SquareFlowLayout/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  SquareFlowLayout
+//
+//  Created by Taras Chernyshenko on 11/11/18.
+//  Copyright © 2018 Taras Chernyshenko. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ SquareFlowLayout/Example/SquareFlowLayout/SquareFlowLayout/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  SquareFlowLayout
+//
+//  Created by Taras Chernyshenko on 11/11/18.
+//  Copyright © 2018 Taras Chernyshenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
